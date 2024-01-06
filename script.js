db055a4505eb1ac4cd9d373bc24569c253320a0b
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-01-06 10:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Function to check if values are of the same type
 function isSameType(value1, value2) {
-    if ((isNaN(value1) && isNaN(value2)) && 
-		typeof(value1) === typeof(value2)) {
+    if ((isNaN(value1) && isNaN(value2)) 
+		|| (typeof(value1) === typeof(value2))) {
         return true;
     }
     return false;
@@ -12,4 +12,4 @@ let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 
 // Check if values are of the same type and display the result
-alert(isSameType(value1, value2));
+alert(isSameType(value1, value2)); 
