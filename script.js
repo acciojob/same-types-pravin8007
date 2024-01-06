9250804775233118ed156da6159f63ea93463795
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-01-06 11:54:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function isSameType(value1, value2) {
   // Check if both values are NaN
   if (isNaN(value1) && isNaN(value2)) {
     return true;
+  }else if(isNaN(value1)){
+	  return false ;
+  } else if(isNaN(value2)){
+	  return false ;
   }
 
   // Check if the types are the same 
