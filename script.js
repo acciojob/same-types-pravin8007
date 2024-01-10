1334a85117acc792a43d128810a39732f8b240e2
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-01-10 22:41:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,21 +9,14 @@ function isSameType(value1, value2) {
 	  return false ;
   }
 
-  // Check if the types are the same 
   if (typeof value1 === typeof value2) {
   	return true ;
   }
 	return false ;
 }
 
-// User input using prompts
 let value1 = prompt("Enter the first value:");
 let value2 = prompt("Enter the second value:");
 
-// Convert user input to appropriate types
-value1 = isNaN(value1) ? value1 : parseInt(value1);
-value2 = isNaN(value2) ? value2 : parseInt(value2);
-
-// Check if values are of the same type and display the result
 alert(isSameType(value1, value2));
  
